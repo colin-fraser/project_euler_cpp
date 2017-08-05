b9dfc1730f2a6ec9705902f27787c01d73f4b55c
Colin <colin.r.fraser@gmail.com>
2017-08-05 16:39:00 -0700

    first

---
+++ cmake-build-debug/CMakeFiles/project_euler_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_euler_cpp.dir/main.cpp.obj"
+  "project_euler_cpp.pdb"
+  "project_euler_cpp.exe"
+  "project_euler_cpp.exe.manifest"
+  "libproject_euler_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_euler_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
